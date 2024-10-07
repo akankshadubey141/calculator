9a4969ab1ef796fc717134153f035c733e081ed2
akankshadubey141 <ankita991199@gmail.com>
2024-10-07 22:20:39 +0530

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,63 @@
+let displayElement = document.getElementById('display');
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        calculate();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = op;
+}
+
+function calculate() {
+    let result;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        case '%':
+            result = prev % current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = result;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    displayElement.innerText = currentOperand;
+}
+
+updateDisplay();
